0853c5fde3afa3f9da7fa9349ba51eacb68c1cf6
leesc0839 <leesc0839@naver.com>
2023-08-21 15:54:29 +0900

    BoardController.java, BoardService.java

--- src/main/java/landvibe/test/controller/BoardController.java
+++ src/main/java/landvibe/test/controller/BoardController.java
@@ -1,8 +1,12 @@
 package landvibe.test.controller;
 
+import landvibe.test.domain.Board;
 import landvibe.test.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -10,4 +14,51 @@ import org.springframework.web.bind.annotation.*;
 //@CrossOrigin(origins = "http://localhost:8080")
 public class BoardController {
     private final BoardService boardService;
+
+    /**
+     * -boards
+     * <p>
+     * 게시글 등록 [post]  /boards/new
+     * (@RequestBody Blog blog 사용)
+     * <p>
+     * 게시글 상세 조회 [get]  /boards/{boardId}
+     * (@PathVariable long boardId 사용)
+     * <p>
+     * 게시글 키워드(제목)로 리스팅 [get]  /boards?keyword=${keyword}
+     * (@RequestParam string keyword 사용)
+     * <p>
+     * 게시글 카테고리(지역)으로 리스팅 [get]  /boards?region=${region}
+     * (@RequestParam string region 사용)
+     */
+
+    @PostMapping("/new")
+    public ResponseEntity<Board> create(@RequestBody Board board) {
+        Long id = boardService.saveBoard(board);
+        Board boardById = boardService.getBoardById(id).get(); // optional , 에러 발생 가능
+        return ResponseEntity.ok(boardById);
+    }
+
+    @GetMapping("/{boardId}")
+    public ResponseEntity<Board> getBoard(@PathVariable Long boardId) {
+        Board boardById = boardService.getBoardById(boardId).get(); // optional, 에러 발생 가능
+        return ResponseEntity.ok(boardById);
+    }
+
+    @GetMapping("/boards/keyword")
+    public ResponseEntity<List<Board>> getBoardsByKeyword(@RequestParam String keyword) {
+        List<Board> boards = boardService.getBoardsByKeyword(keyword);
+        return ResponseEntity.ok(boards); // 비어있는 리스트 반환 가능
+    }
+
+    @GetMapping("/boards/region")
+    public ResponseEntity<List<Board>> getBoardsByRegion(@RequestParam String region) {
+        List<Board> boards = boardService.getBoardsByKeyword(region);
+        return ResponseEntity.ok(boards); // 비어있는 리스트 반환 가능
+    }
+
 }
+
+
+
+
+
--- src/main/java/landvibe/test/service/BoardService.java
+++ src/main/java/landvibe/test/service/BoardService.java
@@ -1,11 +1,40 @@
 package landvibe.test.service;
 
+import landvibe.test.domain.Board;
 import landvibe.test.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class BoardService {
     private final BoardRepository boardRepository;
+
+    @Transactional
+    public Long saveBoard(Board board) {
+        boardRepository.save(board);
+        return board.getBoardId();
+    }
+
+    public List<Board> getAllBoard() {
+        return boardRepository.findAll();
+    }
+
+    public List<Board> getBoardsByKeyword(String keyword) {
+        return boardRepository.findByTitleContaining(keyword);
+    }
+
+    public List<Board> getBoardsByRegion(String region) {
+        return boardRepository.findByRegionContaining(region);
+    }
+
+    public Optional<Board> getBoardById(Long boardId) {
+        return boardRepository.findById(boardId);
+    }
+
 }
