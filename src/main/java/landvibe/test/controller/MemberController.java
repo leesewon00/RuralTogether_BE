eef3fb691cac1236f9085653ce05147422649719
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2023-08-21 12:58:10 +0900

    add member api
    * 첫
    
    * 둘

--- src/main/java/landvibe/test/controller/MemberController.java
+++ src/main/java/landvibe/test/controller/MemberController.java
@@ -1,9 +1,14 @@
 package landvibe.test.controller;
 
+import jakarta.servlet.http.HttpSession;
+import landvibe.test.domain.Member;
 import landvibe.test.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,5 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 //@CrossOrigin(origins = "http://localhost:8080")
 public class MemberController {
     private final MemberService memberService;
+
+    @PostMapping("/new")
+    public ResponseEntity createMember(Member member) {
+        memberService.save(member);
+        return ResponseEntity.ok(memberService.getByEmail(member.getEmail()));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(Member member1, HttpSession session) {
+        Optional<Member> member = memberService.login(member1);
+        if (member.isEmpty()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        session.setAttribute("loginMember", member);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
